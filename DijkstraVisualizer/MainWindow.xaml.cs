91774f092695c3c87214033bffa9f95496b4f96f
BastianLo <bastian.lobe@leibniz-fh.de>
2020-08-23 22:46:13 +0200

    Added Node Connection in object and visual.

--- DijkstraVisualizer/MainWindow.xaml.cs
+++ DijkstraVisualizer/MainWindow.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,70 +15,11 @@ using System.Windows.Shapes;
 
 namespace DijkstraVisualizer
 {
-    class NodeNetwork
-    {
-        private List<Node> Nodes;
-        private Canvas _DrawCanvas;
-
-        public NodeNetwork(Canvas drawCanvas)
-        {
-            Nodes = new List<Node>();
-
-        }
-
-
-        public void AddNode(Node node)
-        {
-            if (CanAddNode(node))
-            {
-                Nodes.Add(node);
-                node.Visualize(_DrawCanvas);
-            }
-        }
-
-        private bool CanAddNode(Node newNode)
-        {
-            var CanAddNode = true;
-            foreach (var node in Nodes)
-            {
-                if (Node.GetDistance(newNode, node) < 50)
-                {
-                    CanAddNode = false;
-                }
-            }
-            Trace.WriteLine(CanAddNode);
-            return CanAddNode;
-        }
-    }
-
-    class Node
-    {
-        private Point _location;
-        public List<Node> Connections;
-
-        public Node(Point location)
-        {
-            _location = location;
-            Connections = new List<Node>();
-        }
-
-        public void Visualize(Canvas drawCanvas)
-        {
-
-        }
-
-        public Point GetLocation() => _location;
-
-        public void SetLocation(Point location) => _location = location;
-
-        public static double GetDistance(Node originNode, Node targetNode)
-            => Math.Sqrt(Math.Pow(originNode._location.X - targetNode._location.X, 2) +
-                         Math.Pow(originNode._location.Y - targetNode._location.Y, 2));
-    }
-
     public partial class MainWindow : Window
     {
         private NodeNetwork MainNetwork;
+        private string buttonSelection;
+        private Node selectedNode;
 
         public MainWindow()
         {
@@ -90,7 +29,97 @@ namespace DijkstraVisualizer
 
         private void DrawCanvas_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
-            MainNetwork.AddNode(new Node(e.GetPosition(this)));
+            if (buttonSelection == "AddNode")
+            {
+                MainNetwork.AddNode(new Node(e.GetPosition(this)));
+            }
+
+            if (buttonSelection == "AddConnection")
+            {
+                //Check if First or Second Click
+                if (selectedNode == null)
+                {
+                    selectedNode = MainNetwork.FindNode(e.GetPosition(this));
+                    if (selectedNode != null)
+                        selectedNode.VisualNode.Fill = new SolidColorBrush(Color.FromRgb(0, 190, 0));
+                }
+                else
+                {
+                    var targetNode = MainNetwork.FindNode(e.GetPosition(this));
+                    //Check if User clicked on a node
+                    if (targetNode != null)
+                    {
+                        Trace.WriteLine("Mouse: " + e.GetPosition(this));
+                        Trace.WriteLine("Node: " + selectedNode.GetLocation());
+
+                        if (Math.Abs(selectedNode.GetLocation().X - e.GetPosition(this).X) < 10 && Math.Abs(selectedNode.GetLocation().Y - e.GetPosition(this).Y) < 10)
+                        {
+                            Trace.WriteLine("ERROR");
+                        }
+                        else
+                        {
+                            //Check if Connection already exists
+                            if (!selectedNode.Connections.Contains(targetNode))
+                            {
+                                //Create Visual Connection
+                                var connection = new Line();
+                                connection.X1 = selectedNode.GetLocation().X;
+                                connection.X2 = targetNode.GetLocation().X;
+                                connection.Y1 = selectedNode.GetLocation().Y;
+                                connection.Y2 = targetNode.GetLocation().Y;
+                                connection.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                                connection.StrokeThickness = 4;
+                                DrawCanvas.Children.Add(connection);
+
+                                //Create Object Connection
+                                targetNode.Connections.Add(selectedNode);
+                                selectedNode.Connections.Add(targetNode);
+
+                                selectedNode.VisualNode.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                                selectedNode = null;
+
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void BtnClearNetwork_OnClick(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure that you want to delete the current Network?", "Delete Network",
+                MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                DrawCanvas.Children.Clear();
+                MainNetwork = new NodeNetwork(DrawCanvas);
+            }
+        }
+
+        private void BtnAddNode_OnClick(object sender, RoutedEventArgs e)
+        {
+            buttonSelection = "AddNode";
+        }
+
+        private void BtnAddConnection_OnClick(object sender, RoutedEventArgs e)
+        {
+            buttonSelection = "AddConnection";
+        }
+
+        private void BtnFindRoute_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainGrid_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selectedNode != null)
+            {
+                selectedNode.VisualNode.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                selectedNode = null;
+            }
         }
     }
 }
---
+++ DijkstraVisualizer/NodeNetwork.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DijkstraVisualizer
+{
+    class NodeNetwork
+    {
+        private List<Node> Nodes;
+        private Canvas _DrawCanvas;
+
+        public NodeNetwork(Canvas drawCanvas)
+        {
+            Nodes = new List<Node>();
+            _DrawCanvas = drawCanvas;
+        }
+
+        public Node FindNode(Point location)
+        {
+            Node outNode = null;
+
+            foreach (var node in Nodes)
+            {
+                if (Math.Sqrt(Math.Pow(node.GetLocation().X - location.X, 2) +
+                              Math.Pow(node.GetLocation().Y - location.Y, 2)) < 12.5)
+                {
+                    outNode = node;
+                    Trace.WriteLine("Node Found");
+                }
+            }
+
+            return outNode;
+        }
+        public void AddNode(Node node)
+        {
+            if (CanAddNode(node))
+            {
+                Nodes.Add(node);
+                node.Visualize(_DrawCanvas);
+            }
+        }
+
+        private bool CanAddNode(Node newNode)
+        {
+            var CanAddNode = true;
+            foreach (var node in Nodes)
+            {
+                if (Node.GetDistance(newNode, node) < 50)
+                {
+                    CanAddNode = false;
+                }
+            }
+            return CanAddNode;
+        }
+    }
+}
